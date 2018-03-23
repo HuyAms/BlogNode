0a2be9d95dad3df0b4832d771cd326a63c1d7b8d
HuyTrinh <dinhhuyams@gmail.com>
2018-03-23 10:21:08 +0200

    fix bug add controller

--- server/api/categories/categoryRoutes.js
+++ server/api/categories/categoryRoutes.js
@@ -1,17 +1,15 @@
 const router = require('express').Router();
-const auth = require('../../auth/auth')
+const auth = require('../../auth/auth');
+const controller = require('./categoryController');
 const checkUser = [auth.decodeToken(), auth.getFreshUser()];
-
 
 router.param('id', controller.params);
 
-router.route('/')
-.get(controller.get)
-.post(checkUser, controller.post)
+router.route('/').get(controller.get).post(checkUser, controller.post);
 
-router.route('/:id')
-.get(controller.getOne)
-.put(checkUser, controller.put)
-.delete(checkUser, controller.delete)
+router.route('/:id').
+    get(controller.getOne).
+    put(checkUser, controller.put).
+    delete(checkUser, controller.delete);
 
 module.exports = router;
--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -1,18 +1,16 @@
 const router = require('express').Router();
-const auth = require('../../auth/auth')
+const auth = require('../../auth/auth');
+const controller = require('./userController');
 const checkUser = [auth.decodeToken(), auth.getFreshUser()];
 
 router.param('id', controller.params);
 router.get('/me', checkUser, controller.me);
 
-router.route('/')
-.get(controller.get)
-.post(controller.post)
+router.route('/').get(controller.get).post(controller.post);
 
-router.route('/:id')
-.get(controller.getOne)
-.put(checkUser, controller.put)
-.delete(checkUser, controller.delete)
-
+router.route('/:id').
+    get(controller.getOne).
+    put(checkUser, controller.put).
+    delete(checkUser, controller.delete);
 
 module.exports = router;
