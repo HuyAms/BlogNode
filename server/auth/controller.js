a4be1d9678cf9b91ede8ae23f567eb4c6ead337f
HuyTrinh <dinhhuyams@gmail.com>
2018-03-25 11:23:17 +0300

    fix authen token

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -5,6 +5,6 @@ exports.signin = (req, res, next) => {
   // req.user will be there from the middleware
   // verify user. Then we can just create a token
   // and send it back for the client to consume
-  const token = signToken(req.user_id);
+  const token = signToken(req.user._id);
   res.json({token: token});
 }
